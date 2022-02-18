f4af298508a7e93c4cca1a12c6e6fee5e72563ad
LePepo <lepepos666@gmail.com>
2022-02-18 20:35:49 -0300

    Avanze en la maestria

--- src/main/java/Eventos/MaestriaExp.java
+++ src/main/java/Eventos/MaestriaExp.java
@@ -1,16 +1,21 @@
 package Eventos;
 
+import Utilidades.Data;
 import Utilidades.Utils;
-import net.minecraft.network.chat.ChatComponentText;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.block.data.BlockData;
+import org.bukkit.Sound;
+import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
 import tlldos.tll2.TLL2;
+
+import java.util.HashMap;
 
 import static Utilidades.Format.format;
 
@@ -26,69 +31,34 @@ public class MaestriaExp implements Listener{
         var p = e.getPlayer();
         var block = e.getBlock();
         var data = p.getPersistentDataContainer();
-        var dataMaestria = data.get(new NamespacedKey(plugin,"maestrialvl"), PersistentDataType.INTEGER);
-        var dataMaestriaExp = data.get(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER);
 
-
-        if(dataMaestriaExp == null){
-            data.set(Utils.key("maestriaexp"),PersistentDataType.INTEGER,0);
+        if (!block.getType().name().toLowerCase().contains("ore")) {
+            return;
         }
 
-        if(block.getType() == Material.COAL_ORE){
-            data.set(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER,dataMaestriaExp + 1);
-            p.sendActionBar(format("&bMineria: " + dataMaestriaExp + " / "));
-        }else if(block.getType() == Material.IRON_ORE){
-            data.set(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER,dataMaestriaExp + 5);
-            p.sendActionBar(format("&bMineria: " + dataMaestriaExp + " / "));
-        }else if(block.getType() == Material.COPPER_ORE){
-            data.set(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER,dataMaestriaExp + 1);
-            p.sendActionBar(format("&bMineria: " + dataMaestriaExp + " / "));
-        }else if(block.getType() == Material.GOLD_ORE){
-            data.set(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER,dataMaestriaExp + 5);
-            p.sendActionBar(format("&bMineria: " + dataMaestriaExp + " / "));
-        }else if(block.getType() == Material.LAPIS_ORE){
-            data.set(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER,dataMaestriaExp + 5);
-            p.sendActionBar(format("&bMineria: " + dataMaestriaExp + " / "));
-        }else if(block.getType() == Material.REDSTONE_ORE){
-            data.set(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER,dataMaestriaExp + 3);
-            p.sendActionBar(format("&bMineria: " + dataMaestriaExp + " / "));
-        }else if(block.getType() == Material.EMERALD_ORE){
-            data.set(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER,dataMaestriaExp + 10);
-            p.sendActionBar(format("&bMineria: " + dataMaestriaExp + " / "));
-        }else if(block.getType() == Material.DIAMOND_ORE){
-            data.set(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER,dataMaestriaExp + 3);
-            p.sendActionBar(format("&bMineria: " + dataMaestriaExp + " / "));
-        }else if(block.getType() == Material.NETHER_QUARTZ_ORE){
-            data.set(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER,dataMaestriaExp + 1);
-            p.sendActionBar(format("&bMineria: " + dataMaestriaExp + " / "));
-        }else if(block.getType() == Material.NETHER_GOLD_ORE){
-            data.set(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER,dataMaestriaExp + 1);
-            p.sendActionBar(format("&bMineria: " + dataMaestriaExp + " / "));
-        }else if(block.getType() == Material.DEEPSLATE_COAL_ORE){
-            data.set(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER,dataMaestriaExp + 3);
-            p.sendActionBar(format("&bMineria: " + dataMaestriaExp + " / "));
-        }else if(block.getType() == Material.DEEPSLATE_COPPER_ORE){
-            data.set(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER,dataMaestriaExp + 3);
-            p.sendActionBar(format("&bMineria: " + dataMaestriaExp + " / "));
-        }else if(block.getType() == Material.DEEPSLATE_IRON_ORE){
-            data.set(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER,dataMaestriaExp + 7);
-            p.sendActionBar(format("&bMineria: " + dataMaestriaExp + " / "));
-        }else if(block.getType() == Material.DEEPSLATE_GOLD_ORE){
-            data.set(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER,dataMaestriaExp + 7);
-            p.sendActionBar(format("&bMineria: " + dataMaestriaExp + " / "));
-        }else if(block.getType() == Material.DEEPSLATE_REDSTONE_ORE){
-            data.set(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER,dataMaestriaExp + 3);
-            p.sendActionBar(format("&bMineria: " + dataMaestriaExp + " / "));
-        }else if(block.getType() == Material.DEEPSLATE_LAPIS_ORE){
-            data.set(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER,dataMaestriaExp + 3);
-            p.sendActionBar(format("&bMineria: " + dataMaestriaExp + " / "));
-        }else if(block.getType() == Material.DEEPSLATE_DIAMOND_ORE){
-            data.set(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER,dataMaestriaExp + 10);
-            p.sendActionBar(format("&bMineria: " + dataMaestriaExp + " / "));
-        }else if(block.getType() == Material.DEEPSLATE_EMERALD_ORE){
-            data.set(new NamespacedKey(plugin,"maestriaexp"),PersistentDataType.INTEGER,dataMaestriaExp + 15);
-            p.sendActionBar(format("&bMineria: " + dataMaestriaExp + " / "));
+        int level = getMasteryLevel(p);
+        int exp = getMasteryExp(p);
+
+        int asd = level - 1;
+
+        p.sendActionBar(format("&bMineria: " + exp + " / "));
+        p.playSound(p.getLocation(), Sound.ENTITY_EXPERIENCE_ORB_PICKUP, 10.0F, 2.0F);
+
+        if (exp >= maxExpNecesary(p)) {
+            setMasteryEXP(p, -maxExpNecesary(p));
+            giveLevel(p, 1);
+
+            p.playSound(p.getLocation(), Sound.BLOCK_RESPAWN_ANCHOR_DEPLETE, 10.0F, 2.0F);
+
+            p.sendTitle(format("&b¡NUEVO NIVEL!"), format("&8" + asd + "&c -> &7" + level));
+
+            Bukkit.getOnlinePlayers().forEach(player -> {
+                player.sendMessage(format("&3MAESTRIA &8> &c&l" + p.getName() + "&7 ha aumentado su nivel. &e" + asd + "&8 >> &6" + level));
+            });
+            return;
         }
+
+        setMasteryEXP(p, getMasteryExp(p) + getGiveExp(block));
 
     }
 
@@ -103,8 +73,116 @@ public class MaestriaExp implements Listener{
         || block.getType() == Material.EMERALD_ORE || block.getType() == Material.DIAMOND_ORE || block.getType() == Material.NETHER_QUARTZ_ORE
         || block.getType() == Material.NETHER_GOLD_ORE || block.getType() == Material.DEEPSLATE_DIAMOND_ORE || block.getType() == Material.DEEPSLATE_EMERALD_ORE
         || block.getType() == Material.DEEPSLATE_LAPIS_ORE || block.getType() == Material.DEEPSLATE_REDSTONE_ORE || block.getType() == Material.DEEPSLATE_GOLD_ORE
-        || block.getType() == Material.DEEPSLATE_IRON_ORE || block.getType() == Material.DEEPSLATE_COPPER_ORE || block.getType() == Material.DEEPSLATE_COAL_ORE){
+        || block.getType() == Material.DEEPSLATE_IRON_ORE || block.getType() == Material.DEEPSLATE_COPPER_ORE || block.getType() == Material.DEEPSLATE_COAL_ORE) {
 
         }
+    }
+
+
+    public int getGiveExp(Block block) {
+        if(block.getType() == Material.COAL_ORE){
+            return 1;
+        }else if(block.getType() == Material.IRON_ORE){
+            return 5;
+        }else if(block.getType() == Material.COPPER_ORE){
+            return 1;
+        }else if(block.getType() == Material.GOLD_ORE){
+            return 5;
+        }else if(block.getType() == Material.LAPIS_ORE){
+            return 5;
+        }else if(block.getType() == Material.REDSTONE_ORE){
+            return 3;
+        }else if(block.getType() == Material.EMERALD_ORE){
+            return 10;
+        }else if(block.getType() == Material.DIAMOND_ORE){
+            return 3;
+        }else if(block.getType() == Material.NETHER_QUARTZ_ORE){
+            return 1;
+        }else if(block.getType() == Material.NETHER_GOLD_ORE){
+            return 1;
+        }else if(block.getType() == Material.DEEPSLATE_COAL_ORE){
+            return 3;
+        }else if(block.getType() == Material.DEEPSLATE_COPPER_ORE){
+           return 3;
+        }else if(block.getType() == Material.DEEPSLATE_IRON_ORE){
+            return 7;
+        }else if(block.getType() == Material.DEEPSLATE_GOLD_ORE){
+            return 7;
+        }else if(block.getType() == Material.DEEPSLATE_REDSTONE_ORE){
+            return 3;
+        }else if(block.getType() == Material.DEEPSLATE_LAPIS_ORE){
+            return 3;
+        }else if(block.getType() == Material.DEEPSLATE_DIAMOND_ORE){
+            return 10;
+        }else if(block.getType() == Material.DEEPSLATE_EMERALD_ORE){
+            return 15;
+        } else {
+            return 0;
+        }
+    }
+
+
+    public int maxExpNecesary(Player p) {
+        return 1000 * getMasteryLevel(p);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    //Mastery Level Desing
+
+
+    public int getMasteryExp(Player p) {
+        PersistentDataContainer data = Data.get(p);
+
+        return data.has(Utils.key("maestriaexp"), PersistentDataType.INTEGER) ? data.get(Utils.key("maestriaexp"), PersistentDataType.INTEGER) : 0;
+    }
+
+    public int getMasteryLevel(Player p) {
+        PersistentDataContainer data = Data.get(p);
+
+        return data.has(Utils.key("maestrialvl"), PersistentDataType.INTEGER) ? data.get(Utils.key("maestrialvl"), PersistentDataType.INTEGER) : 0;
+    }
+
+    public void setMasteryEXP(Player p, int exp) {
+        PersistentDataContainer data = Data.get(p);
+
+        int required = 1000;
+
+        if (exp >= required) {
+            int source = exp / required;
+
+            giveLevel(p, getMasteryExp(p) + source);
+        }
+
+        data.set(Utils.key("maestriaexp"), PersistentDataType.INTEGER, exp);
+    }
+
+    public void setMasteryLevel(Player p, int level) {
+        PersistentDataContainer data = Data.get(p);
+
+        int max = 20;
+
+        if (level >= max) {
+            return;
+        }
+
+        data.set(Utils.key("maestrialvl"), PersistentDataType.INTEGER, level);
+    }
+
+    public void giveLevel(Player p, int var1) {
+        PersistentDataContainer data = Data.get(p);
+
+        int level = data.get(Utils.key("maestriaexp"), PersistentDataType.INTEGER) + var1;
+
+        data.set(Utils.key("maestriaexp"), PersistentDataType.INTEGER, level);
     }
 }
